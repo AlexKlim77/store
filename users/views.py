eeb380f7ecffd3a423107ff9aac4baebf4f2a11f
Alex Klim <alexlsmailik@gmail.com~>
2022-12-30 22:34:18 +0200

    1. UserRegistrationView - use form_class, success_url = reverse_lazy, update context; change url for registration. 2. UserProfileView - get_success_url, update context; in base.html and profile.html - using "user_id" for identification. urls - login_required decorators to view UserProfileView.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
+from django.urls import reverse, reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
 
+from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from products.models import Basket
 
@@ -24,28 +25,53 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 
-def registration(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Ви успішно зареєструвалися')
-            return HttpResponseRedirect(reverse('users:login'))
-    else:
-        form = UserRegistrationForm()
-    context = {'form': form}
-    return render(request, 'users/registration.html', context)
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserRegistrationForm
+    template_name = 'users/registration.html'
+    success_url = reverse_lazy('users:login')
+
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title'] = 'Store - Регістрація'
+        return context
+
+# def registration(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(data=request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Ви успішно зареєструвалися')
+#             return HttpResponseRedirect(reverse('users:login'))
+#     else:
+#         form = UserRegistrationForm()
+#     context = {'form': form}
+#     return render(request, 'users/registration.html', context)
 
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-    else:
-        form = UserProfileForm(instance=request.user)
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
+
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = 'Store - Особистий кабінет'
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
+
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('users:profile'))
+#     else:
+#         form = UserProfileForm(instance=request.user)
 
     # робимо розрахунки для виводу в Шаблон
     # baskets = Basket.objects.filter(user=request.user)
@@ -57,15 +83,15 @@ def profile(request):
     #     total_sum += basket.sum()
     #     total_quantity += basket.quantity
 
-    context = {'title': 'Store - Профиль',
-               'form': form,
-               'baskets': Basket.objects.filter(user=request.user),
-               # 'total_sum': total_sum,
-               # 'total_sum': sum(basket.sum() for basket in baskets),
-               # 'total_quantity': total_quantity
-               # 'total_quantity': sum(basket.quantity for basket in baskets),
-               }
-    return render(request, 'users/profile.html', context)
+    # context = {'title': 'Store - Профиль',
+    #            'form': form,
+    #            'baskets': Basket.objects.filter(user=request.user),
+    #            # 'total_sum': total_sum,
+    #            # 'total_sum': sum(basket.sum() for basket in baskets),
+    #            # 'total_quantity': total_quantity
+    #            # 'total_quantity': sum(basket.quantity for basket in baskets),
+    #            }
+    # return render(request, 'users/profile.html', context)
 
 
 def logout(request):
