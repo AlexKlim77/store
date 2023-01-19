90e472e7bf8e3b09c96e19bfc08dcd0ebca6a5de
Alex Books <alexlsmailik@gmail.com>
2023-01-19 16:26:08 +0200

    add  context_processors create file and add in settings.py remove from users.views def get_context_data(), where add baskets in context. delete comments in users.views.

--- users/views.py
+++ users/views.py
@@ -16,22 +16,6 @@ class UserLoginView(LoginView):
     form_class = UserLoginForm
 
 
-# def login(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = auth.authenticate(username=username, password=password)
-#             if user:
-#                 auth.login(request, user)
-#                 return HttpResponseRedirect(reverse('index'))
-#     else:
-#         form = UserLoginForm()
-#
-#     context = {'form': form}
-#     return render(request, 'users/login.html', context)
-
 
 class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     model = User
@@ -41,18 +25,6 @@ class UserRegistrationView(TitleMixin, SuccessMessageMixin, CreateView):
     success_message = 'Ви успішно зареєструвалися'
     title = 'Store - Регістрація'
 
-
-# def registration(request):
-#     if request.method == 'POST':
-#         form = UserRegistrationForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Ви успішно зареєструвалися')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegistrationForm()
-#     context = {'form': form}
-#     return render(request, 'users/registration.html', context)
 
 
 class UserProfileView(TitleMixin, UpdateView):
@@ -64,46 +36,8 @@ class UserProfileView(TitleMixin, UpdateView):
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data()
-        # context['title'] = 'Store - Особистий кабінет'
-        context['baskets'] = Basket.objects.filter(user=self.object)
-        return context
-
-# @login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#     else:
-#         form = UserProfileForm(instance=request.user)
-
-# робимо розрахунки для виводу в Шаблон
-# baskets = Basket.objects.filter(user=request.user)
-# total_sum = sum(basket.sum() for basket in baskets)
-# total_quantity = sum(basket.quantity for basket in baskets)
-# total_sum = 0
-# total_quantity = 0
-# for basket in baskets:
-#     total_sum += basket.sum()
-#     total_quantity += basket.quantity
-
-# context = {'title': 'Store - Профиль',
-#            'form': form,
-#            'baskets': Basket.objects.filter(user=request.user),
-#            # 'total_sum': total_sum,
-#            # 'total_sum': sum(basket.sum() for basket in baskets),
-#            # 'total_quantity': total_quantity
-#            # 'total_quantity': sum(basket.quantity for basket in baskets),
-#            }
-# return render(request, 'users/profile.html', context)
 
 
-# def logout(request):
-#     auth.logout(request)
-#     return HttpResponseRedirect(reverse('index'))
 
 
 class EmailVerificationView(TitleMixin, TemplateView):
