a948f6049fb3cf572c9e893d2b1e74457167d159
Alex Books <alexlsmailik@gmail.com>
2023-01-24 11:51:06 +0200

    remove mistake insert ;

--- store/settings.py
+++ store/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from db_settings import DATABASES;
+from .db_settings import DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
