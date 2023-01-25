58a32d622cb335c6f3f98e95daca4ca431bd37a9
Alex_Books <alexlsmailik@gmail.com>
2023-01-25 16:25:51 +0200

    Simple test case for check: status code key context_data used Template for response

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+        # print(response)
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context_data['title'], 'Store')
+        self.assertTemplateUsed(response, 'products/index.html')
