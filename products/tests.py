fb891f4affc3c951156a0b8f3901fc76164a9e04
Alex_Books <alexlsmailik@gmail.com>
2023-01-26 11:02:17 +0200

    test output Product and filter on Category. use fixture for load data in testDB. set use var in setUp function.

--- products/tests.py
+++ products/tests.py
@@ -1,6 +1,9 @@
+from http import HTTPStatus
+
 from django.test import TestCase
 from django.urls import reverse
 
+from products.models import Product, ProductCategory
 
 class IndexViewTestCase(TestCase):
 
@@ -12,3 +15,32 @@ class IndexViewTestCase(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertEquals(response.context_data['title'], 'Store')
         self.assertTemplateUsed(response, 'products/index.html')
+
+class ProductListViewTestCase(TestCase):
+    fixtures = ['fixtures/categories.json', 'fixtures/goods.json']
+
+    def setUp(self) -> None:
+        self.products = Product.objects.all()
+
+    def test_list(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        self.assertEquals(response.status_code, HTTPStatus.OK)
+        self.assertEquals(response.context_data['title'], 'Store - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[:3]))
+
+    def test_list_with_category(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_id': category.id})
+        response = self.client.get(path)
+
+        products = Product.objects.all()
+        self.assertEquals(response.status_code, HTTPStatus.OK)
+        self.assertEquals(response.context_data['title'], 'Store - Каталог')
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category_id=category.id))
+        )
