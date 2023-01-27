f4635426215ff5bb7d38826f3ae0b4fcf8be816f
Alex_Books <alexlsmailik@gmail.com>
2023-01-27 10:58:01 +0200

    test User Registration: test get templates. test create user(post method) - redirect, exist new user.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from users.forms import UserRegistrationForm
+from users.models import User
+
+
+class sernameUserRegistrationViewTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.data = {
+            'first_name': 'Valk', 'last_name': 'Kalin',
+            'username': 'alesa', 'email': 'sdert@gmail.com',
+            'password1': '12345678Qw', 'password2': '12345678Qw',
+        }
+
+        self.path = reverse('users:registration')
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Store - Регістрація')
+        self.assertTemplateUsed(response, 'users/registration.html')
+
+    def test_user_registration_post(self):
+        username = self.data['username']
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        # check creating of user
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        # for debug mode
+        # a=1
