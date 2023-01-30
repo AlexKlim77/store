516f75f3eb75ba6fe06291f9bbd712646f60e782
Alex_Books <alexlsmailik@gmail.com>
2023-01-30 12:19:21 +0200

    work with Redis - Cache, with 3 case. add requirment_pack.txt.

--- products/urls.py
+++ products/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 
 from django.urls import path
+# from django.views.decorators.cache import cache_page  # для кешировання всієї сторінки
 
 from .views import *
 
@@ -22,6 +23,7 @@ app_name = 'products'
 
 urlpatterns = [
 
+    # path('', cache_page(30) (ProductListView.as_view()), name='index'),  # кешировання всієї сторінки на 30 сек
     path('', ProductListView.as_view(), name='index'),
     path('category/<int:category_id>/', ProductListView.as_view(), name='category'),
     path('page/<int:page>/', ProductListView.as_view(), name='paginator'),
