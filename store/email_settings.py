c0c9a7234c7b3925d33ede9ae60c274149def551
Alex_Books <alexlsmailik@gmail.com>
2023-01-30 13:35:58 +0200

    create 1 task for celery - sen email verification.

--- store/email_settings.py
+++ store/email_settings.py
@@ -1,6 +1,8 @@
 # Optional SMTP authentication information for EMAIL_HOST.
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# need change for work with celery, with real mail-server
+
 # for work with Out-Mail-Server
 EMAIL_HOST = ''  # for smtp/imap - out
 EMAIL_HOST_USER = ''
