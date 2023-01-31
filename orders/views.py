d387971d9bc864b45609c264d5cac9b7452d4644
Alex_Books <alexlsmailik@gmail.com>
2023-01-31 17:12:31 +0200

    add forms orders

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic.edit import  CreateView
+from django.views.generic.edit import CreateView
 
+from orders.forms import OrderForm
 
 class OrderCreateViews(CreateView):
     template_name = 'orders/order-create.html'
-
+    form_class = OrderForm
