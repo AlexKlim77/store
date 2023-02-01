7a548177431cb4dce8823505c1492050330a2df5
Alex_Books <alexlsmailik@gmail.com>
2023-02-01 16:13:53 +0200

    add api-keys for stripe. need to install..

--- store/settings.py
+++ store/settings.py
@@ -212,3 +212,10 @@ CACHES = {
 #  Celery
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+
+# Stripe
+STRIPE_PUBLIC_KEY = ''
+STRIPE_SECRET_KEY = ''
+
+
+
